b55660d23c005d67307b47bc285bf3c147158e79
Ramon <Jokmenen@gmail.com>
2018-04-06 15:07:04 +0200

    Promotieaanvraag Plugin

---
+++ pravajson.php
@@ -0,0 +1,78 @@
+<html>
+    <body>
+<?php
+ /*
+   Plugin Name: Flow Promotion Request
+   */
+   
+function parsePOST($post){	
+	$format = "%s : %s ";
+
+	echo "Verzonden: <br>";
+
+	foreach (array_keys($post) as $element){
+		
+		$myObj->$element = $post[$element];
+	
+	}
+
+	$myJSON = json_encode($myObj);
+
+	echo $myJSON;
+}
+
+function sendJSONMail($JSON){
+	// Mail aanvraag
+	require_once('PHPMailer-master/class.phpmailer.php');
+
+	$file_to_attach = $JSON;
+
+
+	$bodytext = "Dear Web Commissioner!
+
+	[commissaris] wants to hand in a promotion request for [activiteit]. You can find the promotion request, as well as optionally an .ics file for the digital agenda implementation. 
+
+	Kind regards,
+	Sheldon.
+	";
+
+
+	$email = new PHPMailer();
+	$email->From      = 'info@svflow.nl';
+	$email->FromName  = 'Promotieaanvraag Distributer';
+	$email->Subject   = 'Promotieaanvraag'; //van maken Promotion Request [naam activiteit] [commissaris]
+	$email->Body      = $bodytext;
+	$email->AddAddress( 'vicevoorzitter@svflow.nl', 'meerminman' );
+	$email->AddCC( 'website@svflow.nl', 'mosseljongeee' );
+
+	$email->AddStringAttachment( $file_to_attach , 'aanvraag.json');
+	print($email->Send());
+
+	return 0;
+
+	}   
+   
+   
+   
+// Vars halen uit post
+
+//JSON
+
+
+if ($_POST["fromPage"]==1){
+		
+	$json = parsePOST($_POST);
+	sendJSONMail($json);
+
+
+}
+else {
+	
+	echo "ERROR, Please use the Promotion Request Form";
+	return 1;
+}
+
+// You can contact Ramon Pieternella (jokmenen@gmail.com) for any questions about this promotion request system. Please add "Flow Promotion Request" as a subject. 
+?>
+    </body>
+</html>
